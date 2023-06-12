cf085994755423d0473aa380f47bb85b6aecedf8
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-13 00:10:44 +0530

    Update Unique.cpp

--- Unique.cpp
+++ Unique.cpp
@@ -7,3 +7,11 @@ int findUnique(int *arr, int size)
     }
     return sum;
 }
+// for(int i=0;i<arr.size();i++){
+//         int c=0;
+//         for(int j=0;j<arr.size();j++){
+//             if(arr[i]=arr[j])
+//             c++;
+//         }
+//         return arr[c];
+//     }
