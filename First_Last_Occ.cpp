efcfc9ab2fcf7b947cde61803965819718a0be17
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-14 17:00:05 +0530

    Create First_Last_Occ.cpp

---
+++ First_Last_Occ.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int lastOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+int firstOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main(){
+	int arr[6]={2,3,3,3,41,5};
+	cout<<"first occurance is"<<firstOcc(arr,6,3)<<endl;
+
+	cout<<"last occurance is"<<lastOcc(arr,6,3);
+	return 0;
+	
+}
