d03f32482b09069fff3ee6866aee02c96f544533
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-14 19:23:30 +0530

    Create Pivot_ele.cpp

---
+++ Pivot_ele.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int pivot(int arr[],int n){
+	int s=0;
+	int e=n-1;
+	while(s<e){
+		int mid=s+(e-s)/2;
+		if(arr[mid]>=arr[0]){
+			s=mid+1;
+			
+		}
+		else{
+			e=mid;
+		}
+		
+	}
+	return s;
+}
+
+
+int main(){
+	int arr[6]={111,82,12,17,10,11};
+   cout<<"the pivot elemet index is"<<pivot(arr,6);
+	return 0;
+	
+}
